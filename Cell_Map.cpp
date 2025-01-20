a9218b0ca3f33bcf788f862c840e399dad804b83
ユーザー名 <>
2025-01-20 21:01:17 +0900

    remove pragma omp parallel for at createParticleMap func because the original code I created didn't use it. Don't understand why but probably double accsess to the memory occuring.

--- Cell_Map.cpp
+++ Cell_Map.cpp
@@ -7,7 +7,6 @@ void createParticleMap(const std::vector<Particle>& particles, int N, int L,
     cells.resize(L, std::vector<std::vector<Particle>>(L));
 
     // 各パーティクルに対して、セルの位置を計算し、そのセルに格納
-    #pragma omp parallel for
     for (int i = 0; i < N; i++) {
         int cell_x = static_cast<int>(std::floor(particles[i].x)) % L;
         int cell_y = static_cast<int>(std::floor(particles[i].y)) % L;
