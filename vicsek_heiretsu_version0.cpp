175562d489b5c4ff97b9a08622f4c865626016a1
ユーザー名 <>
2025-01-20 21:57:11 +0900

    successed also getNC.h

--- vicsek_heiretsu_version0.cpp
+++ vicsek_heiretsu_version0.cpp
@@ -9,26 +9,7 @@
 #include <chrono>
 #include "Particle.h"
 #include "dist.h"
-
-// double dist(Particle p1, Particle p2)
-// {
-//     return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-// }
-
-std::vector<std::array<int,2>> getNeighboringCell(int cell_x, int cell_y, int L)
-{
-    std::vector<std::array<int,2>> neighbors;
-    for (int dx = -1; dx <= 1; dx++)
-    {
-        for (int dy = -1; dy <= 1; dy++)
-        {
-            int neighbor_x = (cell_x + dx + L) % L;
-            int neighbor_y = (cell_y + dy + L) % L;
-            neighbors.push_back({neighbor_x, neighbor_y});
-        }
-    }
-    return neighbors;
-}
+#include "getNeighboringCell.h"
 
 int main()
 {
